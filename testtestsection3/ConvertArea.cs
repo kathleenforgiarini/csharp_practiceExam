ed538410b0b375fced486bacde037391de72126b
Kathleen Forgiarini da Silva <82175930+kathleenforgiarini@users.noreply.github.com>
2023-02-28 10:43:01 -0500

    Add files via upload

---
+++ testtestsection3/ConvertArea.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testtestsection3
+{
+    internal class ConvertArea
+    {
+        // private field
+        private double metricArea;
+
+        // public property
+        public double MetricArea
+        {
+            get
+            {
+                return metricArea;
+            }
+            set
+            {
+                metricArea = value;
+            }
+        }
+
+        // default constructor
+        public ConvertArea()
+        {
+            this.MetricArea = 0;
+        }
+        public double cmToIn()
+        {
+            return metricArea * 0.1550;
+        }
+
+        public double mToYd()
+        {
+            return metricArea * 1.1960;
+        }
+
+        public double haToAcres()
+        {
+            return metricArea * 2.4711;
+        }
+        public double kmToMile()
+        {
+            return metricArea * 0.3861;
+        }
+    }
+}
---
+++ testtestsection3/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// CODED BY KATHLEEN FORGIARINI DA SILVA
+// 02-28-2023
+// SIMULATION MIDTERM SECTION 3, converting area from metric to US or Imperial
+
+namespace testtestsection3
+{
+    
+    public partial class frmAreaConvert : Form
+    {
+        public frmAreaConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAreaConvert_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+            textBox6.Text = "0";
+            textBox7.Text = "0";
+            textBox8.Text = "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = Convert.ToDouble(textBox1.Text);
+                ConvertArea ca1 = new ConvertArea();
+                ca1.MetricArea = value;
+                textBox5.Text = ca1.cmToIn().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oh no! An exception occurred - Details: " + ex.Message);
+                textBox1.Focus();
+            }
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = Convert.ToDouble(textBox2.Text);
+                ConvertArea ca2 = new ConvertArea();
+                ca2.MetricArea = value;
+                textBox6.Text = ca2.mToYd().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oh no! An exception occurred - Details: " + ex.Message);
+                textBox2.Focus();
+            }
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = Convert.ToDouble(textBox3.Text);
+                ConvertArea ca3 = new ConvertArea();
+                ca3.MetricArea = value;
+                textBox7.Text = ca3.haToAcres().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oh no! An exception occurred - Details: " + ex.Message);
+                textBox3.Focus();
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = Convert.ToDouble(textBox4.Text);
+                ConvertArea ca4 = new ConvertArea();
+                ca4.MetricArea = value;
+                textBox8.Text = ca4.kmToMile().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oh no! An exception occurred - Details: " + ex.Message);
+                textBox4.Focus();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+            textBox6.Text = "0";
+            textBox7.Text = "0";
+            textBox8.Text = "0";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("You want to quit?", "Exit", MessageBoxButtons.OKCancel).ToString() == "OK")
+            {
+                MessageBox.Show("You decide to quit");
+                this.Close();
+            } else
+            {
+                textBox1.Text = "0";
+                textBox2.Text = "0";
+                textBox3.Text = "0";
+                textBox4.Text = "0";
+                textBox5.Text = "0";
+                textBox6.Text = "0";
+                textBox7.Text = "0";
+                textBox8.Text = "0";
+            }
+        }
+    }
+}
